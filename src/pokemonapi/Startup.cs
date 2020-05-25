89b6c2780cd0ea9eabf3bb5559681104ad839494
thyagofalconi <thyagofalconi@gmail.com>
2020-05-25 13:15:25 -0300

    Commit

--- src/pokemonapi/Startup.cs
+++ src/pokemonapi/Startup.cs
@@ -13,7 +13,7 @@ namespace pokemonapi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration)  
         {
             Configuration = configuration;
         }
